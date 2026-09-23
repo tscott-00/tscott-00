de8fd10af48dbf265193434e816952dbfcdeaa4d
Thomas A. Scott <81178161+tscott-00@users.noreply.github.com>
2026-09-23 12:13:05 -0500

    update publications and profile

--- src/research/Research.vue
+++ src/research/Research.vue
@@ -16,13 +16,52 @@
         <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Publications</li>
         <li class="list-row"><div>
           <div class="font-semibold">
+            Dimension Bridging for 3D RANS with Neural Network Accelerated Gaussian Functional Regression
+          </div>
+          <div class="italic text-xs pl-2">
+            Wesley Lao, <span class="text-secondary">Thomas A. Scott</span>, Tan Bui-Thanh, Paul Laiu, and Matt Bement
+          </div>
+          <div class="text-xs pl-2">
+            2026
+            <a href="https://arxiv.org/abs/2608.27639" target="_blank" rel="noopener noreferrer" class="font-bold text-primary hover:underline">[Preprint]</a>
+          </div>
+        </div></li>
+        <li class="list-row"><div>
+          <div class="font-semibold">
             Rendezvous Planning from Sparse Observations of Optimally Controlled Targets
           </div>
-          <div class="italic text-sm">
-            Thomas A. Scott, Lukas Taus, Yen-Hsi Richard Tsai, Tan Bui-Thanh, Justin G.R. Delva
+          <div class="italic text-xs pl-2">
+            <span class="text-secondary">Thomas A. Scott</span>, Lukas Taus, Yen-Hsi Richard Tsai, Tan Bui-Thanh, and Justin G.R. Delva
           </div>
-          <a href="https://drive.google.com/file/d/1KNtBeFSynPbC5P4646PCkE0jzYyuk_s1/view?usp=sharing" class="font-bold text-primary hover:underline">[Preprint]</a>
-          </div></li>
+          <div class="text-xs pl-2">
+            2026
+            <a href="https://arxiv.org/abs/2604.01428" target="_blank" rel="noopener noreferrer" class="font-bold text-primary hover:underline">[Preprint]</a>
+          </div>
+        </div></li>
+        <li class="list-row"><div>
+          <div class="font-semibold">
+            An optimal control approach for neural network architecture adaptation with a posteriori error estimation
+          </div>
+          <div class="italic text-xs pl-2">
+            C G Krishnanunni, <span class="text-secondary">Thomas A. Scott</span>, and Tan Bui-Thanh
+          </div>
+          <div class="text-xs pl-2">
+            2026
+            <a href="https://arxiv.org/abs/2607.07637" target="_blank" rel="noopener noreferrer" class="font-bold text-primary hover:underline">[Preprint]</a>
+          </div>
+        </div></li>
+        <li class="list-row"><div>
+          <div class="font-semibold">
+            Trust-Region Approaches for High-Order Node Movement
+          </div>
+          <div class="italic text-xs pl-2">
+            Devina P. Sanjaya, <span class="text-secondary">Thomas A. Scott</span>, and Carl F. Ollivier-Gooch
+          </div>
+          <div class="text-xs pl-2">
+            2026
+            <a href="https://arc.aiaa.org/doi/10.2514/6.2026-0764" target="_blank" rel="noopener noreferrer" class="font-bold text-primary hover:underline">[AIAA]</a>
+          </div>
+        </div></li>
       </ul>
     </div>
   </main>
